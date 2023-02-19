e7a6afe8fe081391b39ba64caf8caa14d88f3cd6
dachi-meparishvili <dachi.meparishvili7@gmail.com>
2023-02-19 16:38:00 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.round(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = parseInt(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('Please enter a Number');
+
+    //when the user wins
+  } else if (guess === secretNumber) {
+    displayMessage('correct Number!🙌');
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High📈' : 'Too Low📉');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You Lost💀💀💀');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.round(Math.random() * 20) + 1;
+
+  displayMessage('Start Guessing...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
